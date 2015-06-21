4d1fe346c76e6ed94c2ca123fdca0fa7841191e1
SwarnalakshmiBalasubramamaniam <swarnalakshmi.b@gmail.com>
2015-06-22 00:16:22 +0530

    modified as per assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This creates the matrix and calculates the solve and stores both in the global variables
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get<- function()x
+  setsolve <- function (solve) m <<- solve
+  getsolve <- function () m
+  list (set = set, 
+        get = get, 
+        setsolve = setsolve, 
+        getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## This reads the cache, and if it is empty it calls the makecachematrix to get the solve value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if (!is.null(m))
+  {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
+  m
 }
